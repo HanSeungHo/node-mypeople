37731a2e7085089e241bb93fa6255cf2589e5963
한승호 <hansh@daumcorp.com>
2013-06-04 15:44:14 +0900

    v0.0.2 commit

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+<<<<<<< HEAD
+exports.index = function(req, res){
+	console.log('receive');
+ 	res.render('index', { title: '마이피플 봇 예제' });
+=======
+exports.index = function(req, res){
+	console.log('receive');
+ 	res.render('index', { title: '마이피플 봇 예제' });
+>>>>>>> 76ca4a58ced85433b596cb32c173de3f48d510a4
+};
